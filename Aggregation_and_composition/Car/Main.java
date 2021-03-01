beb211290a63cbff60818649675b7f156fab4cec
Alex221291 <75179749+Alex221291@users.noreply.github.com>
2021-03-01 20:36:10 +0300

    Задачи_ Aggregation_and_composition

--- Aggregation_and_composition/Car/Main.java
+++ Aggregation_and_composition/Car/Main.java
@@ -1,1 +1,15 @@
-sdfgdfg
+package aggregation.car;
+//2. Создать объект класса Автомобиль, используя классы Колесо, Двигатель. Методы: ехать, заправляться,
+//менять колесо, вывести на консоль марку автомобиля.
+public class Main {
+    public static void main(String[] args) {
+        Car car = new Car("Maserati");
+        car.printCarModel();
+        car.go();
+        car.stop();
+        car.refuel();
+        car.swapWheel(1, new Wheel());
+        car.go();
+    }
+} 
+
